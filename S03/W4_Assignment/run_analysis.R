21eea78451d7a669e0693660239637bf869d4318
Kieso Jan <kieso.jan@outlook.com>
2021-06-22 09:11:49 +0800

    to add detail comments for providing clear meaning of each code

--- S03/W4_Assignment/run_analysis.R
+++ S03/W4_Assignment/run_analysis.R
@@ -26,34 +26,61 @@ unzip(zipfile = "dataFiles.zip")
 
 
 # Load activity labels + features ####
-path <- getwd()
+path <- getwd() #get current working folder
+
+#read activity name from txt file and assign column name "classLabels" & "activityName" respectively
 activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt")
                         , col.names = c("classLabels", "activityName"))
+
+#read feature Names from "features.txt" and assign column name "index" & "featureNames respectively
 features <- fread(file.path(path, "UCI HAR Dataset/features.txt")
                   , col.names = c("index", "featureNames"))
+
+#To get index value by extract "featureNames" value matching "mean" or "std" from dataframe "features"
 featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
+#apply the output as filter to get exact name of featurenames relates to "mean" or "std"
 measurements <- features[featuresWanted, featureNames]
+#remove '()' character
 measurements <- gsub('[()]', '', measurements)
 
+
 # Load train datasets ####
+
+#creating train with only mean & std variables by reading file "X_train.txt" & applying index filter "featuresWanted"
 train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
+#rename train column by applying cleaned measurement name 
 data.table::setnames(train, colnames(train), measurements)
+
+#reading file "Y_train.txt" with name column name "Activity"
 trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt")
                          , col.names = c("Activity"))
+
+#reading file "subject_train.txt" with name column name "SubjectNum"
 trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt")
                        , col.names = c("SubjectNum"))
+#combine dataset "trainActivities" & "trainSubjects" to train by following sequence.
 train <- cbind(trainSubjects, trainActivities, train)
 
 # Load test datasets ####
+
+#creating test dataset with only mean & std variables by reading file "X_test.txt" & applying index filter "featuresWanted"
 test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
+
+#rename test column by applying cleaned measurement name 
 data.table::setnames(test, colnames(test), measurements)
+
+#reading file "Y_test.txt" with name column name "Activity"
 testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
                         , col.names = c("Activity"))
+
+#reading file "subject_test.txt" with name column name "SubjectNum"
 testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt")
                       , col.names = c("SubjectNum"))
+
+#combine dataset "testActivities" & "testSubjects" to test by following sequence.
 test <- cbind(testSubjects, testActivities, test)
 
-# merge datasets ####
+# merge train & test datasets ####
 combined <- rbind(train, test)
 
 # Convert classLabels to activityName basically. More explicit. 
@@ -62,9 +89,14 @@ combined[["Activity"]] <- factor(combined[, Activity]
                                  , labels = activityLabels[["activityName"]])
 
 combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
+
+#  From the data set above "combined"
+# creates a second, independent tidy data set with the average of each variable 
+#for each activity and each subject.
 combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
 combined <- reshape2::dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
 
+#Writing tidydata to target folder with assigned file name 
 data.table::fwrite(x = combined, file = "output/tidyData.txt", quote = FALSE, row.names = FALSE)
 
 
